2088781ece4a172bde93cccb429e08118ff0f87b
lumin <928696088@qq.com>
2024-03-28 08:51:43 +0800

    first commit

---
+++ src/main/java/com/lumin/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lumin.git;/*
+ *@Title:GitTest
+ *@Descripton:com.lumin.git
+ *@Auther:www.baidu.com
+ *@Version:1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git......");
+    }
+}
