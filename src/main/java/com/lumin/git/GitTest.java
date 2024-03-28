dddba172d57de938980d74e568c0e4ad1e433dd6
lumin <928696088@qq.com>
2024-03-28 11:23:56 +0800

    master commit2

--- src/main/java/com/lumin/git/GitTest.java
+++ src/main/java/com/lumin/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello Git4......");
         System.out.println("Hello Git5......");
         System.out.println("Hello Git5......");
+        System.out.println("master test......");
     }
 }
