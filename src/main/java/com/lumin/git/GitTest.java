2943e5e8012b126a3615acc1585400c9521bb2f8
lumin <928696088@qq.com>
2024-03-28 08:56:03 +0800

    second commit

--- src/main/java/com/lumin/git/GitTest.java
+++ src/main/java/com/lumin/git/GitTest.java
@@ -8,5 +8,7 @@ package com.lumin.git;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git......");
+        System.out.println("Hello Git2......");
+        System.out.println("Hello Git3......");
     }
 }
